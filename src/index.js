1f39c2842ad2ded2694f3184c51b64120108db81
Alejandro_Ferrera <aferrerah1421@gmail.com>
2022-06-01 04:43:20 -0400

    im tired

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Amplify from 'aws-amplify';
+import {Auth} from 'aws-amplify';
 import config from './aws-exports';
-Amplify.configure(config);
+Auth.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
