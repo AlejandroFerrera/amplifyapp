5a68d2255c493ff91fa04ee2a97b42760ac354ec
Alejandro_Ferrera <aferrerah1421@gmail.com>
2022-06-02 14:37:16 -0400

    changed the mail text

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,8 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
         "amplifyapp18da8dab18da8dab": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
             "UserPoolId": "string",
             "UserPoolArn": "string",
             "UserPoolName": "string",
