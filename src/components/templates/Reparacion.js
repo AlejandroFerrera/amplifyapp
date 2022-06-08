8477181b7cada7f97812862d8cea767f327ed45e
Alejandro_Ferrera <aferrerah1421@gmail.com>
2022-06-08 00:03:13 -0400

    added repair section

--- src/components/templates/Reparacion.js
+++ src/components/templates/Reparacion.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuthenticator } from "@aws-amplify/ui-react";
+import { Form } from "react-bootstrap";
+import { FormButton } from "../molecules/FormButton";
 
 export function Reparacion() {
 
@@ -8,7 +10,23 @@ export function Reparacion() {
     ]);
 
     return <>
-        <h1 className="welcome">Hola {user.attributes.given_name} {user.attributes.family_name}</h1>
-        <h3 className="welcome">Se encuentra en la seccion para solicitar una reparacion</h3>
+        <h1 className="welcome">Hola {user.attributes.given_name}</h1>
+        <h3 className="table-title">Solicite una reparación</h3>
+        <Form className="w-50 mx-auto repair-form">
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Requerimiento</Form.Label>
+                <Form.Select aria-label="Default select example">
+                    <option value="1">Cambio de frenos</option>
+                    <option value="2">Centrado de llanta</option>
+                    <option value="3">Ajustar los cambios</option>
+                    <option value="4">Mantenimiento general</option>
+                </Form.Select>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Observaciones</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <FormButton text="Solicitar reparación"/>
+        </Form>
     </>
 }
